48445c25d0271053b7980b4770f44d94e089529f
Luis Ernesto Zorrilla PIla <lezorrilla1992@gmail.com>
2023-03-31 12:26:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Conectado a la BD Mysql");
+});
+
+app.get('/',(req, res)=>{
+    connection.query('SELECT * FROM tabla',(error, results, fields)=>{
+        if(err) throw error;
+        res.send(results);
+    });
+});
+app.listen(port,() =>{
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
